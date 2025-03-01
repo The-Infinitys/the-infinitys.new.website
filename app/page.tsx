d9685677f39b9c30699a8275078ff068ea668f63
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-03-01 08:35:48 +0000

    グローバルCSSのリセット、アニメーションの追加、ページスタイルの設定、及び新しい画像コンポーネントの導入

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import type { Metadata } from "next";
+import "./page.css";
+
+// Import Images
+import infinite_icon from "../public/infinite-icon.svg";
+import infinite_logo_white from "../public/infinite-logo-white.svg";
+import infinite_logo_black from "../public/infinite-logo-black.svg";
 
 export const metadata: Metadata = {
   title: "The Infinity's",
@@ -9,7 +15,15 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-    <h1>Hello, World!</h1>
+      <section className="first">
+      <Image className="inf-icon" src={infinite_icon} alt="" />
+      <Image className="dark-mode inf-logo" src={infinite_logo_white} alt="" />
+      <Image className="light-mode inf-logo" src={infinite_logo_black} alt="" />
+      </section>
+      <section className="about">
+        <h2>About Us</h2>
+        <p>We are a team of passionate developers and designers.</p>
+      </section>
     </>
   );
 }
