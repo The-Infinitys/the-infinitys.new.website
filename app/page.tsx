6ae38f2598aeacfeb391840d9d864c550c6b1ad1
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2025-03-04 02:29:34 +0000

    スタイルの調整とレスポンシブデザインの追加、アニメーションの修正

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import "./page.css";
 import infinite_icon from "../public/infinite-icon.svg";
 import infinite_logo_white from "../public/infinite-logo-white.svg";
 import infinite_logo_black from "../public/infinite-logo-black.svg";
-import the_infinitys_logo from "../public/home/The-Infinitys.png"
+import the_infinitys_logo from "../public/home/The-Infinitys.png";
 
 export const metadata: Metadata = {
   title: "The Infinity's",
@@ -16,24 +16,16 @@ export const metadata: Metadata = {
 function contents() {
   return (
     <>
-      <section className="contents" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Image
-          style={{
-            width: "20vw",
-            height: "20vw",
-            aspectRatio: "1/1",
-            borderRadius: "10vw",
-            marginRight: "5vw",
-            marginLeft: "5vw",
-          }}
-          src={the_infinitys_logo} alt={""} />
-        <div style={{ width: "50vw" }}>
+      <section className="contents description">
+        <Image id="description-inf-logo" src={the_infinitys_logo} alt={""} />
+        <div>
           <h1>こんにちは! The Infinity's です!</h1>
           <p>趣味がプログラミングと楽器演奏の無限です！</p>
           <p>主にプログラミングについて投稿します！</p>
         </div>
       </section>
-    </>);
+    </>
+  );
 }
 
 export default function Home() {
@@ -41,8 +33,16 @@ export default function Home() {
     <>
       <div className="first-view">
         <Image className="inf-icon" src={infinite_icon} alt="" />
-        <Image className="dark-mode inf-logo" src={infinite_logo_white} alt="" />
-        <Image className="light-mode inf-logo" src={infinite_logo_black} alt="" />
+        <Image
+          className="dark-mode inf-logo"
+          src={infinite_logo_white}
+          alt=""
+        />
+        <Image
+          className="light-mode inf-logo"
+          src={infinite_logo_black}
+          alt=""
+        />
       </div>
       {contents()}
     </>
