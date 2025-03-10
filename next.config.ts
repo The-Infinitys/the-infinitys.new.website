6e43eba9bca687af09ca9ed4147df14a470eace1
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2025-03-10 11:54:22 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  images: {
+    unoptimize: true,
+  },
 };
 
 export default nextConfig;
